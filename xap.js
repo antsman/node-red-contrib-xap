2cadcc5c0d3f4727202e1f1cf233bcf55283e28c
Aivo Antsman <antsman@users.noreply.github.com>
2016-02-26 22:04:00 +0200

    Check options exist

--- xap.js
+++ xap.js
@@ -29,32 +29,26 @@ module.exports = function(RED) {
         }
 
         this.on("input", function(msg) {
-            var uid = n.uid || msg.options.uid;
-            var cls = n.class || msg.options.class;
-            var src = n.source || msg.options.source;
-            var tgt = n.target || msg.options.target;
-            var mgt = n.messagetype || msg.options.messagetype;
-            var brc = n.broadcast || msg.options.broadcast;
-            var prt = n.port || msg.options.port;
-/*
-            node.log(RED._("uid " + uid));
-            node.log(RED._("cls " + cls));
-            node.log(RED._("src " + src));
-            node.log(RED._("tgt " + tgt));
-*/
-            var message = {};
+            var o = msg.options || {},
+                uid = n.uid || o.uid,
+                cls = n.class || o.class,
+                src = n.source || o.source,
+                tgt = n.target || o.target,
+                mgt = n.messagetype || o.messagetype,
+                brc = n.broadcast || o.broadcast,
+                prt = n.port || o.port,
+                options = {'uid': uid, 'class': cls, 'source': src, 'target': tgt, 'broadcast': brc, 'port': prt},
+                message = {};
+
             if (n.message != "") {
                 message = JSON.parse(n.message);
             } else if (msg.hasOwnProperty("payload")) {
                 message = msg.payload;
             }
-            var options = {'uid': uid, 'class': cls, 'source': src, 'target': tgt, 'broadcast': brc, 'port': prt};
+
             var xAPBroadcaster = new xap.XAPBroadcaster(options);
             xAPBroadcaster.send(mgt, message);
-/*
-            node.log(RED._("xap.opt " + JSON.stringify(options)));
-            node.log(RED._("xap.msg " + JSON.stringify(message)));
-*/
+
             if (n.showcount) {
                 this.status({fill: "grey", shape: "ring", text: "count: " + ++count});
             }
